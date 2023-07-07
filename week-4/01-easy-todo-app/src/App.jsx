daffd11800c1153ba5e7882855bfb30542760dff
asbar <asbar03KK@gmail.com>
2023-07-07 17:59:58 +0530

    compleated the assingment 01-easy-todo-app with adding and deleteing todos

--- week-4/01-easy-todo-app/src/App.jsx
+++ week-4/01-easy-todo-app/src/App.jsx
@@ -1,27 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useCallback, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState([])
-    // fetch all todos from server
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/todos")
+      .then((res) => res.json())
+      .then((data) => setTodos(data));
+  }, []);
+
+  function deleteTodo(id) {
+    fetch("http://localhost:3000/todos/" + id, {
+      method: "DELETE",
+    }).then(() => {
+      fetch("http://localhost:3000/todos")
+        .then((res) => res.json())
+        .then((data) => setTodos(data));
+    });
+  }
+
+  async function addTodo() {
+    const title = document.getElementById("title").value;
+    const description = document.getElementById("description").value;
+
+    await fetch("http://localhost:3000/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title,
+        description,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setTodos([...todos, data]);
+      });
+
+    // setTodos([...todos, { title, description }]);
+  }
 
   return (
     <>
       <div>
         <h1>Easy Todo App</h1>
-        <input type="text" />
+        <input type="text" placeholder="title" id="title" />
+        <input type="text" placeholder="description" id="description" />
+        <button onClick={addTodo}>Add To-Do</button>
       </div>
+      <table>
+        <tbody>
+          {todos.map((todo, index) => {
+            console.log("Rendering todos:", todos);
+            return (
+              <Todo
+                key={index}
+                title={todo.title}
+                description={todo.description}
+                deleteTodo={() => deleteTodo(todo.id)}
+              />
+            );
+          })}
+        </tbody>
+      </table>
     </>
-  )
+  );
 }
 
 function Todo(props) {
-    // Add a delete button here so user can delete a TODO.
-    return <div>
-        {props.title}
-    </div>
+  return (
+    <>
+      <tr>
+        <td>{props.title}</td>
+        <td>{props.description}</td>
+        <td>
+          <button onClick={props.deleteTodo}>Delete To-Do</button>
+        </td>
+      </tr>
+    </>
+  );
 }
 
-export default App
+export default App;
